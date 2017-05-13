3fc83bfbcb84ed701e879aa63b818ead1d4bba9a
Ben Pirt <ben@pirt.co.uk>
2017-05-13 15:13:33 +0100

    Uncomment some lines in bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,8 +2,8 @@
 
 SETUP_LOC=/usr/local/headlessPi/setup
 
-#apt-get update
-#apt-get install -y git ansible
+apt-get update
+apt-get install -y git ansible
 mkdir -p $SETUP_LOC
 echo -e "[headlessPi]\nraspberrypi ansible_connection=local" > /tmp/ansible-hosts
 ansible-pull -o -C master -d $SETUP_LOC -i /tmp/ansible-hosts -U https://github.com/headlessPi/headlessPi-setup.git
