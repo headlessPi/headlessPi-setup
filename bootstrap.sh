a8bcdf5b016b4e5c164ca548ffff25282df546a9
Ben Pirt <ben@pirt.co.uk>
2017-05-17 11:33:30 +0100

    Tidy up after bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,5 @@ apt-get install -y git ansible
 mkdir -p $SETUP_LOC
 echo -e "[headlessPi]\nraspberrypi ansible_connection=local" > /tmp/ansible-hosts
 ansible-pull -o -C master -d $SETUP_LOC -i /tmp/ansible-hosts -U https://github.com/headlessPi/headlessPi-setup.git
+rm /tmp/ansible-hosts
+apt-get clean
