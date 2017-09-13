a14f1a2a1e42e0ec69e38d97f456d0b2ec0e64ca
Ben Pirt <ben@pirt.co.uk>
2017-09-13 17:59:22 +0100

    Force run ansible even if the repo hasn't changed

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,6 @@ apt-get update
 apt-get install -y git ansible
 mkdir -p $SETUP_LOC
 echo -e "[headlessPi]\nraspberrypi ansible_connection=local" > /tmp/ansible-hosts
-ansible-pull -o -C master -d $SETUP_LOC -i /tmp/ansible-hosts -U https://github.com/headlessPi/headlessPi-setup.git
+ansible-pull -f -o -C master -d $SETUP_LOC -i /tmp/ansible-hosts -U https://github.com/headlessPi/headlessPi-setup.git --extra-vars "upgrade_val=false"
 rm /tmp/ansible-hosts
 apt-get clean
